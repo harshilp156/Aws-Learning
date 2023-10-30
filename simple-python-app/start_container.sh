305ce52537b85c9fd854b28f9232689079e5004e
harshilp156 <67538347+harshilp156@users.noreply.github.com>
2023-10-30 14:35:40 -0400

    another 1

--- simple-python-app/start_container.sh
+++ simple-python-app/start_container.sh
@@ -2,7 +2,7 @@
 set -e
 
 # Pull the Docker image from Docker Hub
-docker pull harshilrpatel/simple-python-flask-app v    
+docker pull harshilrpatel/simple-python-flask-app
 
 # Run the Docker image as a container
 docker run -d -p 5000:5000 docker pull harshilrpatel/simple-python-flask-app 
